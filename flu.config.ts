dddc186faa50b21c52e0dbdb79dc0c73138cc404
kstdx <kstdx@users.noreply.github.com>
2023-03-02 17:33:00 +0900

    update example

--- flu.config.ts
+++ flu.config.ts
@@ -2,7 +2,7 @@ import { pluginNote, pluginZenn } from './core/plugins.js'
 import { defineConfig } from './core/config.js'
 
 export default defineConfig({
-    title: 'My Hub',
+    title: 'My Flu App',
     description:
         'I have uploaded the repository of this web application to GitHub.',
     username: 'kstdx',
@@ -10,11 +10,7 @@ export default defineConfig({
     links: {
         twitter: 'https://twitter.com/kstdx',
         github: 'https://github.com/kstdx',
-        others: [
-            'https://note.com/kstdx',
-            'https://zenn.dev/kstdx',
-            'https://scratch.mit.edu/users/kst_dx'
-        ]
+        others: ['https://kstdx.com']
     },
     plugins: [
         [pluginNote, 'kstdx'],
